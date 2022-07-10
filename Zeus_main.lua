d855ac5f68de8adf19b4ae4dd11b3753b1a538bb
Zeus Security <Redsec1337@protonmail.com>
2022-07-09 18:46:32 -0700

    update
    update comment

--- Zeus_main.lua
+++ Zeus_main.lua
@@ -4,7 +4,7 @@ if zeus_version then
 end 
 
 --Set Version Here requeriment for the script to work
-zeus_version = "20.55"
+zeus_version = "20.56"
 
 menu.create_thread(function()
 
@@ -6465,17 +6465,17 @@ menu.add_feature("Safe RockStarGames Refund 5.1M", "toggle", money.id, function(
         script.set_global_i(1964179, 5)
         system.wait(1100)
         script.set_global_i(1964179, 4)
-        system.wait(1100)
+        system.wait(2100)
         script.set_global_i(1964179, 3)
-        system.wait(1100)
+        system.wait(2100)
         script.set_global_i(262145+22853 , 133377)
-        system.wait(1100)
+        system.wait(2100)
         script.set_global_i(1964179, 2)
         system.wait(1100)
         script.set_global_i(1964179, 1)
         system.wait(1100)
         script.set_global_i(1964179, 0)
-        system.wait(50000)
+        system.wait(60000)
         
         if not pid.on then
             return HANDLER_CONTINUE
